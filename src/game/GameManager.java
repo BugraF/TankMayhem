14f8de8baa7eabaf9aa2dd6764dddce32823e44d
burakgok <burakgok95@gmail.com>
2016-12-02 00:02:35 +0300

    Netbeans Configuration

---
+++ src/game/GameManager.java
@@ -0,0 +1,16 @@
+package game;
+
+/**
+ *
+ * @author Burak Gök
+ */
+public class GameManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
