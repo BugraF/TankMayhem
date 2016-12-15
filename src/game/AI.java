65c016d8acae4d75b292c7c08bcd9cfda1a998ca
burakgok <burakgok95@gmail.com>
2016-12-15 20:41:59 -0300

    Super constructor added

--- src/game/AI.java
+++ src/game/AI.java
@@ -6,28 +6,36 @@ package game;
  * @author Haluk İncidelen
  */
 public abstract class AI extends Player {
+
+    public AI(String name, String mode, int color) {
+        super(name, mode, color);
+    }
+    
     /**
      * AI plays its turn
      */
-    public void play ( ){
-        
+    public void play() {
+        // TODO Haluk: Implement AI.play()
     }
+    
     /**
      * AI analyzes the game and chooses a path according to the other player’s situations
      */
-    abstract void analyze ( );	
+    abstract void analyze();
+    
     /**
      * AI purchases the optimal bomb or bombs in that situation
      */
-    abstract void purchase ( );
+    abstract void purchase();
     
     /**
      * AI moves if necessary (If there are power-up or if there are an obstacle)
      */
-    abstract protected void move ( );
+    abstract protected void move();
+    
     /**
      * AI fires bomb or uses a power-up
      */
-    abstract protected void interact ( );	
+    abstract protected void interact();
 
 }
