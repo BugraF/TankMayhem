17c7c40ed40ee395fa662d3472c673bfa185a77e
bugrafelekoglu <bugrafelekoglu@gmail.com>
2016-12-16 03:33:19 +0200

    ListButton implemented and added to NewGameMenu

---
+++ src/gui/ListButton.java
@@ -0,0 +1,181 @@
+package gui;
+
+import gui.core.*;
+import java.util.ArrayList;
+import java.util.List;
+import processing.core.PFont;
+import processing.core.PGraphics;
+import processing.core.PImage;
+import processing.event.KeyEvent;
+
+/**
+ * Selectable list button
+ * 
+ * @see gui.core.Button
+ * 
+ * @author Buğra Felekoğlu
+ */
+public class ListButton extends InteractiveComponent {
+    
+    /**
+     * Current state of this button.
+     *      - 0: normal
+     *      - 1: both rollOn and toggled
+     */
+    private int state = 0;
+    
+    /**
+     * String property of the button
+     */
+    private String attribute;
+    
+    /**
+     * Drawn string on the button
+     */
+    private String text;
+    
+    /**
+     * Font type of the String printed
+     */
+    private PFont font;
+    
+    /**
+     * Status of the button
+     */
+    private boolean isSelected = false;
+    
+    /**
+     * The objects that listen to the invocation of this button.
+     */
+    private final List<ActionListener> actionListeners = new ArrayList<>(1);
+    
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+    public String getAttribute() {
+        return attribute;
+    }
+    
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+    
+    public boolean isSelected(){
+        return isSelected;
+    }
+    
+    public void setSelected(boolean select) {
+        if(select){
+            bounds[0] = bounds[0] - width/7;
+            isSelected = select;
+            state = 1;
+        }
+        else{
+            bounds[0] = bounds[0] + width/7;
+            isSelected = false;
+            state = 0;
+        }
+    }
+    
+    public void setFont(PFont font){
+        this.font = font;
+    }
+    
+    @Override
+    public void draw(PGraphics g) {
+        g.textFont(font, 25);
+        g.noStroke();
+        
+        if (state == 0){
+            g.fill(0, 192);
+            g.quad(width/12, 0, width, 0, width, height, 0, height);
+            g.fill(255);
+            g.textAlign(g.CENTER, g.CENTER);
+            g.text(text, width/2-10, height/2-5);
+        }
+        else {
+            g.fill(82, 195, 42, 192);
+            g.quad(width/12, 0, width, 0, width, height, 0, height);
+            g.fill(0);
+            g.textAlign(g.CENTER, g.CENTER);
+            g.text(text, width/2-10, height/2-5);
+        }
+    }
+    
+    /**
+     * Associates the specified action listener to this button.
+     * @return False if this button is already associated to the specified
+     *         action listener, true otherwise.
+     */
+    public boolean addActionListener(ActionListener listener) {
+        if (actionListeners.contains(listener))
+            return false;
+        actionListeners.add(listener);
+        return true;
+    }
+    
+    /**
+     * Disassociates the specified action listener from this button.
+     * @return False if the specified action listener is not associated to this
+     *         button, true otherwise.
+     */
+    public boolean removeActionListener(ActionListener listener) {
+        return actionListeners.remove(listener);
+    }
+    
+    private void invoke() {
+        for (ActionListener listener : actionListeners)
+            listener.actionPerformed(this);
+    }
+    
+    @Override
+    public boolean handleMouseEvent(MouseEvent event) {
+        if (event.getAction() != processing.event.MouseEvent.EXIT
+                && !consumeEvent(event)) return false;
+        if (!enabled) return true;
+        return propagateMouseEvent(this, event);
+    }
+    
+    private boolean consumeEvent(MouseEvent e) {
+        if (isSelected)
+            return false;
+        else
+            return true;
+    }
+
+    @Override
+    public boolean mousePressed(MouseEvent e) {
+        state = 1;
+        return true;
+    }
+
+    @Override
+    public boolean mouseReleased(MouseEvent e) {
+        state = 1;
+        return true;
+    }
+    
+    @Override
+    public boolean mouseClicked(MouseEvent e) {
+        if(!isSelected){
+            setSelected(true);
+            invoke();
+        }
+        return true;
+    }
+    
+    @Override
+    public boolean mouseEntered(MouseEvent e) {
+        if(!isSelected)
+            state = 1;
+        return true;
+    }
+
+    @Override
+    public boolean mouseExited(MouseEvent e) {
+        if(!isSelected)
+            state = 0;
+        return true;
+    }
+}
--- src/gui/NewGameMenu.java
+++ src/gui/NewGameMenu.java
@@ -5,7 +5,10 @@ import gui.core.Component;
 import gui.core.ActionListener;
 import gui.core.Button;
 import gui.core.Parent;
+import java.util.ArrayList;
+import java.util.List;
 import processing.core.PApplet;
+import processing.core.PFont;
 import processing.core.PImage;
 
 /**
@@ -19,8 +22,9 @@ public class NewGameMenu extends Parent implements ActionListener{
      * Background Image
      */
     private PImage background; 
+    private PFont font;
     
-    
+    private String map = "desert";
     
     /**
      * Buttons
@@ -28,18 +32,25 @@ public class NewGameMenu extends Parent implements ActionListener{
     private final Button start_btn = new Button();
     private final Button back_btn = new Button();
     
-//    private final Button back_btn = new Button();
-//    private final Button back_btn = new Button();
-//    private final Button back_btn = new Button();
-//    private final Button back_btn = new Button();
+    private final ListButton snowy = new ListButton();
+    private final ListButton rainy = new ListButton();
+    private final ListButton desert = new ListButton();
+    private final ListButton sunny = new ListButton();
     
+    private final List<ListButton> listButtons = new ArrayList<>(4);
     
     @Override
     public void init(PApplet context) {
+        font = context.createFont("font/seguibl.ttf", 40);
+        
         background = context.loadImage("background/new_game_menu.png");
         setBackground(background);
         
-        add(start_btn, back_btn);
+        add(start_btn, back_btn, snowy, rainy, desert, sunny);
+        listButtons.add(snowy);
+        listButtons.add(rainy);
+        listButtons.add(desert);
+        listButtons.add(sunny);
         
         start_btn.setStateImages(context
                 .loadImage("component/button/start_btn.png"), true);
@@ -47,7 +58,7 @@ public class NewGameMenu extends Parent implements ActionListener{
         start_btn.setLocation(980, 648);
         start_btn.setSize(300, 120);
         start_btn.addActionListener(this);
-        start_btn.setMnemonic(83);    // mnemonic => s
+        start_btn.setMnemonic(10);    // mnemonic => ENTER
         
         back_btn.setStateImages(context
                 .loadImage("component/button/back_btn.png"), true);
@@ -55,7 +66,42 @@ public class NewGameMenu extends Parent implements ActionListener{
         back_btn.setLocation(0, 688);
         back_btn.setSize(200, 80);
         back_btn.addActionListener(this);
-        back_btn.setMnemonic(66);    // mnemonic => b
+        back_btn.setMnemonic(27);    // mnemonic => ESC
+        
+        snowy.setFont(context.createFont("Segoe UI Black", 35));
+        snowy.setAttribute("snowy");
+        snowy.setText("SNOWY MOUNTAINS");
+        snowy.setFont(font);
+        snowy.setLocation(970, 198);
+        snowy.setSize(360, 90);
+        snowy.addActionListener(this);
+        
+        rainy.setFont(context.createFont("Segoe UI Black", 35));
+        rainy.setAttribute("rainy");
+        rainy.setText("RAINY HILLS");
+        rainy.setFont(font);
+        rainy.setLocation(970, 303);
+        rainy.setSize(360, 90);
+        rainy.addActionListener(this);
+        
+        desert.setFont(context.createFont("Segoe UI Black", 35));
+        desert.setAttribute("desert");
+        desert.setText("HOT DESERT");
+        desert.setFont(font);
+        desert.setLocation(970, 408);
+        desert.setSize(360, 90);
+        desert.setSelected(true);
+        desert.addActionListener(this);
+        
+        sunny.setFont(context.createFont("Segoe UI Black", 35));
+        sunny.setAttribute("sunny");
+        sunny.setText("SUNNY FOREST");
+        sunny.setFont(font);
+        sunny.setLocation(970, 513);
+        sunny.setSize(360, 90);
+        sunny.addActionListener(this);
+        
+        
     }
     
     @Override
@@ -63,11 +109,19 @@ public class NewGameMenu extends Parent implements ActionListener{
         if(comp == start_btn){
             ((ScreenManager)getContext())
                     .switchScreen(ScreenManager.SCREEN_GAME);
-//            GameManager.getInstance().startNewGame("Desert", );
+//            GameManager.getInstance().startNewGame(map, );
         }
-            
-        if(comp == back_btn)
+        else if(comp == back_btn){
             ((ScreenManager)getContext())
                     .switchScreen(ScreenManager.SCREEN_MAIN_MENU);
+        }
+        else if(comp instanceof ListButton){
+            for(ListButton btn : listButtons)
+                if(btn.isSelected())
+                    btn.setSelected(false);
+            ((ListButton)comp).setSelected(true);
+            ((ListButton)comp).getAttribute();
+        }
+            
     }
 }
