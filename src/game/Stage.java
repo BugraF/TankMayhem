14598342c8e71878953ce37e359e9ef0ea19cb39
burakgok <burakgok95@gmail.com>
2016-12-17 00:29:31 -0300

    Handles tank movement, camera shift was fixed.

--- src/game/Stage.java
+++ src/game/Stage.java
@@ -13,18 +13,28 @@ import processing.event.KeyEvent;
  * @author Burak Gök
  */
 public class Stage extends InteractiveComponent {
-
+    /** Context of this bomb */
+    private final Game game;
     private World world;
+    
     private Decoration decoration;
+    private Terrain terrain;
     private final Renderer renderer = new Renderer();
-    private final int[] camBounds = new int[4], // x1, y1, x2, y2 (l, t, r, b)
-                        cam = new int[2], camNext = new int[2]; // x, y (center)
+    
+    /** Camera Bounds (x1, y1, x2, y2) -> (l, t, r, b) */
+    private final int[] camBounds = new int[4];
+    /** Camera Focus (x, y) */
+    private final float[] cam = new float[2], camNext = new float[2];
     
     private Interaction interaction;
     private int z_order;
     
-    private final TracerInteraction tracer = new TracerInteraction();
+//    private final TracerInteraction tracer = new TracerInteraction();
     private boolean tracerEnabled = false;
+    
+    public Stage(Game game) {
+        this.game = game;
+    }
     
     /**
      * Creates a reference to the game world.
@@ -38,12 +48,20 @@ public class Stage extends InteractiveComponent {
         this.decoration = decoration;
     }
     
+    void setTerrain(Terrain terrain) {
+        this.terrain = terrain;
+    }
+    
     Renderer getRenderer() {
         return renderer;
     }
     
     Interaction getInteraction() {
         return interaction;
+    }
+    
+    int[] getCameraBounds() {
+        return camBounds;
     }
     
     /**
@@ -55,6 +73,10 @@ public class Stage extends InteractiveComponent {
     void setInteraction(int z_order, Interaction interaction) {
         this.z_order = z_order;
         this.interaction = interaction;
+    }
+    
+    public void finalizeInteraction() {
+        interaction._finalize();
     }
     
     /**
@@ -83,13 +105,24 @@ public class Stage extends InteractiveComponent {
      * The specified point is used as the focus of the camera.
      */
     void setCamera(int x, int y) {
-        hWidth = width / 2;
-        hHeight = height / 2;
-        
         camNext[0] = PApplet.constrain(x, hWidth, world.width() - hWidth);
         camNext[1] = PApplet.constrain(y, hHeight, world.height() - hHeight);
         
         updateCamera = true;
+//        System.out.format("Next Camera: [%s, %s]\n", camNext[0], camNext[1]);
+    }
+
+    @Override
+    public void setSize(int width, int height) {
+        super.setSize(width, height);
+        hWidth = width / 2;
+        hHeight = height / 2;
+        if (camBounds[2] + camBounds[3] == 0) { // Initialization
+            cam[0] = hWidth;
+            cam[1] = hHeight;
+        }
+        else// Resize
+            setCamera((int)cam[0], (int)cam[1]);
     }
     
     // TODO Another graphics object should be passed to this method since
@@ -97,44 +130,70 @@ public class Stage extends InteractiveComponent {
     @Override
     public void draw(PGraphics g) {
         if (shiftCamera) {
-            cam[0] = (int)PApplet.lerp(cam[0], camNext[0], 0.1f);
-            cam[1] = (int)PApplet.lerp(cam[1], camNext[1], 0.1f);
-            if (cam[0] == camNext[0] && cam[1] == camNext[1])
-                shiftCamera = updateCamera = false;
+            cam[0] = PApplet.lerp(cam[0], camNext[0], 0.1f);
+            cam[1] = PApplet.lerp(cam[1], camNext[1], 0.1f);
+            if (Math.abs(cam[0] - camNext[0]) < 1 
+                    && Math.abs(cam[1] - camNext[1]) < 1)
+                shiftCamera = false;
+//            System.out.format("Camera: [%s, %s]\n", cam[0], cam[1]);
         } else if (updateCamera) {
             cam[0] = camNext[0];
             cam[1] = camNext[1];
             updateCamera = false;
         }
         if (updateCamera) {
-            camBounds[0] = cam[0] - hWidth;
-            camBounds[1] = cam[1] - hHeight;
-            camBounds[2] = cam[0] + hWidth;
-            camBounds[3] = cam[1] + hHeight;
+            camBounds[0] = (int)cam[0] - hWidth;
+            camBounds[1] = (int)cam[1] - hHeight;
+            camBounds[2] = (int)cam[0] + hWidth;
+            camBounds[3] = (int)cam[1] + hHeight;
         }
-        g.translate(camBounds[0], camBounds[1]);
         
         decoration.drawBackground(g, camBounds);
-        if (tracerEnabled)
-            tracer.draw(g);
-        if (z_order < 0)
-            interaction.draw(g);
+        g.translate(-camBounds[0], -camBounds[1]);
+        
+//        if (tracerEnabled)
+//            tracer.drawBehindTerrain(g);
+        interaction.drawBehindTerrain(g);
+        terrain.draw(g, camBounds);
+        interaction.drawAfterTerrain(g);
         renderer.draw(g, camBounds);
+        
+        g.translate(camBounds[0], camBounds[1]);
         decoration.drawForeground(g, camBounds);
-        if (z_order >= 0)
-            interaction.draw(g);
     }
     
     @Override
     public void handleKeyEvent(KeyEvent event) {
-        if (enabled)
+        if (enabled) {
+            propagateKeyEvent(this, event);
             propagateKeyEvent(interaction, event);
+        }
     }
     
     @Override
     public boolean handleMouseEvent(MouseEvent event) {
         if (!enabled) return true;
         return propagateMouseEvent(interaction, event);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == 65) // A
+            game.getActiveTank().moveLeft();
+        else if (e.getKeyCode() == 68) // D
+            game.getActiveTank().moveRight();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == 65 || e.getKeyCode() == 68) {
+            Tank tank = game.getActiveTank();
+            if (e.getKeyCode() == 65) // A
+                tank.stopLeft();
+            else // D
+                tank.stopRight();
+            shiftCamera((int)tank.getX(), (int)tank.getY());
+        }
     }
     
 }
