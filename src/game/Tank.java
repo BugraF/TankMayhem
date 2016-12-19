68c53dd782188821eca67f066365d8aff25302fe
burakgok <burakgok95@gmail.com>
2016-12-18 22:18:08 -0300

    Updates the collision mask when needed. Removes itself from the world when destroyed. Does not move when it runs out of fuel.

--- src/game/Tank.java
+++ src/game/Tank.java
@@ -2,6 +2,7 @@ package game;
 
 import game.engine.PhysicsObj;
 import game.engine.RenderObj;
+import game.engine.World;
 import game.engine.WorldObj;
 import processing.core.PGraphics;
 import processing.core.PImage;
@@ -13,6 +14,7 @@ import processing.core.PImage;
 public class Tank implements PhysicsObj, WorldObj, RenderObj {
     /** The context of this tank */
     private final Game game;
+    private final World world;
     
     /** Position of this tank */
     private float x, y;
@@ -40,24 +42,28 @@ public class Tank implements PhysicsObj, WorldObj, RenderObj {
     
     /* Image & Bounds */
     private final PImage image; // without barrel
-    private final int barrel; // barrel starting position relative to the center
+    private final int barrel; // barrel pivot translation
     private final int[] bounds = new int[4];
     
-    private int terrainMask;
-    private int tanksMask;
-    private int tankHeight;
-    private int tankWidth;
+    /* Last position */
+    private float lastX, lastY;
+    
+    private final int terrainMask;
     
     public Tank(Game game, PImage image, int barrel, int color) {
         this.game = game;
         this.image = image;
         this.barrel = barrel;
         this.color = color;
+        
+        this.world = game.getWorld();
+        terrainMask = world.generateCheckMask(game.getTerrain());
     }
     
-    public void init(int x, int y, float damageBonus, float shieldBonus) {
-        this.x = x;
-        this.y = y;
+    void init(int x, int y, float damageBonus, float shieldBonus) {
+        this.x = lastX = x;
+        this.y = lastY = y;
+        updateBounds();
         this.damageBonus = damageBonus;
         this.shieldBonus = shieldBonus;
     }
@@ -87,24 +93,37 @@ public class Tank implements PhysicsObj, WorldObj, RenderObj {
         return true; // TODO Burak: Bound checking for Tank
     }
 
-    public void moveLeft() {
-	goLeft = true;
+    void moveLeft() {
+        goLeft = true;
     }
-    public void moveRight() {
+    void moveRight() {
         goRight = true;
     }
-    public void stopLeft() {
+    void stopLeft() {
 	goLeft = false;
     }
-    public void stopRight() {
-        goRight = false;    
+    void stopRight() {
+        goRight = false;
     }
     
     /**
      * Updates the health points of this tank by the specified amount.
      */
-    public void updateHp(float delta){
-	hp += delta;
+    void updateHp(float delta) {
+        float newHP = hp + delta;
+        hp = newHP < 0 ? 0 : (newHP > 100 ? 100 : newHP);
+        if (hp == 0)
+            game.getWorld().remove(this);
+	else
+            hp = newHP > 100 ? 100 : newHP;
+    }
+    
+    /**
+     * Updates the fuel of this tank by the specified amount.
+     */
+    void updateFuel(float delta) {
+        float newFuel = fuel + delta;
+        fuel = newFuel < 0 ? 0 : (newFuel > 100 ? 100 : newFuel);
     }
     
     /**
@@ -114,7 +133,7 @@ public class Tank implements PhysicsObj, WorldObj, RenderObj {
      *                than 1 should be specified. Similarly, to speed up the
      *                tank, a value greater than 1 is used.
      */
-    public void setAgility(float agility){
+    void setAgility(float agility) {
         this.agility = agility;
     }
     
@@ -129,15 +148,17 @@ public class Tank implements PhysicsObj, WorldObj, RenderObj {
         return fuel;
     }
     
-    float getDamageBonus() {
+    public float getDamageBonus() {
         return damageBonus;
     }
-    float getShieldBonus() {
+    public float getShieldBonus() {
         return shieldBonus;
     }
      
     @Override
     public void checkConstraints() { // world border, movement, hold surface
+        boolean update = lastX != x || lastY != y;
+        
 //        if(getX()<0){
 //            setX(0);
 //        }
@@ -149,7 +170,7 @@ public class Tank implements PhysicsObj, WorldObj, RenderObj {
 //        }
         
         // Movement
-        if (goLeft ^ goRight) { // Either of them is true, but not both
+        if (goLeft ^ goRight && fuel > 0) { // Either of them is true, but not both
             velX = goLeft ? -30 : 30;
             fuel = Math.max(fuel - 0.05f, 0);
         }
@@ -176,6 +197,14 @@ public class Tank implements PhysicsObj, WorldObj, RenderObj {
 //            if (getVx() < 500)
 //                setVy(getVy()+40); 
 //        }
+
+        if (update) {
+            updateBounds();
+            if (hp != 0)
+                world.updateMask(this, null);
+        }
+        lastX = x;
+        lastY = y;
     }
     
     @Override
@@ -186,6 +215,15 @@ public class Tank implements PhysicsObj, WorldObj, RenderObj {
     @Override
     public int[] getBounds() {
         return bounds;
+    }
+    
+    private void updateBounds() {
+        bounds[0] = (int)x - image.width / 2;
+        bounds[1] = (int)y - image.height / 2;
+        bounds[2] = bounds[0] + image.width; // odd width
+        bounds[3] = bounds[1] + image.height; // odd height
+//        bounds[2] = (int)x + image.width / 2;
+//        bounds[3] = (int)y + image.height / 2;
     }
 
     private void rotate(float delta) {
@@ -206,8 +244,8 @@ public class Tank implements PhysicsObj, WorldObj, RenderObj {
     public float getVx() { return velX; }
     public float getVy() { return velY; }
     
-    public void setX(float x) { this.x = x; }
-    public void setY(float y) { this.y = y; }
+    public void setX(float x) { this.x = x; /*updateBounds();*/ }
+    public void setY(float y) { this.y = y; /*updateBounds();*/ }
     public void setVx(float vX) { this.velX = vX; }
     public void setVy(float vY) { this.velY = vY; }
     
