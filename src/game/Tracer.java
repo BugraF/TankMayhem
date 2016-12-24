ea9c475a26a25f29cd00ac4c01ed354514521898
burakgok <burakgok95@gmail.com>
2016-12-24 06:17:49 -0300

    Tracer was animated.

--- src/game/Tracer.java
+++ src/game/Tracer.java
@@ -11,7 +11,8 @@ public class Tracer {
     /** Context of this interaction */
     private final Game game;
     
-    private final static float timeStep = 0.025f;
+    private final static float timeStep = 0.05f;
+    private float phase = 0;
     
     public Tracer(Game game) {
         this.game = game;
@@ -35,8 +36,12 @@ public class Tracer {
         float velY = 1500 * tank.firePower * sin;
         
         int wind = game.getPhysicsEngine().getWind() * 5; // 5: wind multiplier
+//        timeStep += timeStepDelta;
+//        if (timeStep < 0.05f || timeStep > 0.08f)
+//            timeStepDelta = -timeStepDelta;
         float x2 = x1 + velX * timeStep;
         float y2 = y1 + velY * timeStep;
+        phase = (phase + 0.05f) % 1f;
         velX += wind * timeStep;
         velY += 980 * timeStep;
         float x3 = x2 + velX * timeStep;
@@ -51,7 +56,9 @@ public class Tracer {
             float x4 = x3 + velX * timeStep;
             float y4 = y3 + velY * timeStep;
             if (dash)
-                g.curve(x1, y1, x2, y2, x3, y3, x4, y4);
+//                g.curve(x1, y1, x2, y2, x3, y3, x4, y4);
+                g.curve(x1, y1, x1 + (x3 - x1) * phase, y1 + (y3 - y1) * phase,
+                        x2 + (x4 - x2) * phase, y2 + (y4 - y2) * phase, x4, y4);
             x1 = x2; y1 = y2;
             x2 = x3; y2 = y3;
             x3 = x4; y3 = y4;
