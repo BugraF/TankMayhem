5ec53073d1e1a64d249d0973cd178efe10d2a8da
bugrafelekoglu <bugrafelekoglu@gmail.com>
2016-12-24 23:18:44 +0200

    End Game Frame created, detects winner and returns to main menu

---
+++ src/gui/EndGameFrame.java
@@ -0,0 +1,50 @@
+package gui;
+
+import game.Player;
+import gui.core.Component;
+import processing.core.PApplet;
+import processing.core.PGraphics;
+import processing.core.PImage;
+
+/**
+ * End Game Frame of the game.
+ * 
+ * @author Bugra Felekoglu
+ */
+public class EndGameFrame extends Frame {
+    
+    private PImage background;
+    private Player winner;
+    
+    public void setWinner(Player player) {
+        winner = player;
+    }
+    
+    @Override
+    public void init(PApplet context){
+        super.init(context);
+        setSize(1000, 700);
+        setLocation(140, 34);
+        background = context.loadImage("background/end_game.png");
+        setBackground(background);
+    }
+    
+    @Override
+    public void draw(PGraphics g) {
+        g.pushMatrix();
+        g.fill(0xFFECC93F);
+        g.textSize(200);
+        g.textAlign(g.CENTER, g.BASELINE);
+        g.text(winner.getName(), 500, 480);
+        g.popMatrix();
+    }
+    
+    @Override
+    public void actionPerformed(Component comp) {
+        if (comp == this.closeBtn) {
+            ((ScreenManager)getContext()).closeFrame();
+            ((ScreenManager)getContext())
+                    .switchScreen(ScreenManager.SCREEN_MAIN_MENU);
+        }
+    }
+}
