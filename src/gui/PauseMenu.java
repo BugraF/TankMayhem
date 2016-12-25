c8b31cda9d35b2e391032d1d72e912815ed61874
bugrafelekoglu <bugrafelekoglu@gmail.com>
2016-12-25 02:31:23 +0200

    Pause frame modified: main menu and help buttons arranged

--- src/gui/PauseMenu.java
+++ src/gui/PauseMenu.java
@@ -4,6 +4,7 @@ import gui.core.Component;
 import gui.core.ActionListener;
 import gui.core.Button;
 import processing.core.PApplet;
+import processing.core.PImage;
 
 /**
  * Pause frame, includes: 
@@ -17,39 +18,55 @@ import processing.core.PApplet;
 public class PauseMenu extends Frame implements ActionListener {
 
     private final Button helpBtn = new Button();
-    private final Button exitBtn = new Button();
-
-    // TODO add music slider and sound slider
+    private final Button mainMenuBtn = new Button();
+    private final Slider musicSlider = new Slider();
+    private final Slider soundSlider = new Slider();
+    
+    private PImage background;
     
     @Override
     public void init(PApplet context){
+        super.init(context);
         setSize(430, 520);
-//        helpBtn.setStateImages(context
-//                .loadImage("component/button/help_btn_small.png"), true);
-        helpBtn.setFreeShape(true);
-        helpBtn.setLocation(70, 290);
+        setLocation(425, 60);
+        background = context.loadImage("background/pause_menu.png");
+        setBackground(background);
+        
+        add(helpBtn, mainMenuBtn);
+        
+        helpBtn.setStateImages(context
+                .loadImage("component/button/help_btn_small.png"), true);
+        helpBtn.setLocation(65, 290);
         helpBtn.setSize(300, 90);
         helpBtn.addActionListener(this);
+        helpBtn.setMnemonic(72);    // Mnemonic => H
         
-//        exitBtn.setStateImages(context
-//                .loadImage("component/button/exit_btn.png"), true);
-        exitBtn.setFreeShape(true);
-        exitBtn.setLocation(70, 400);
-        exitBtn.setSize(300, 90);
-        exitBtn.addActionListener(this);
+        mainMenuBtn.setStateImages(context
+                .loadImage("component/button/main_menu_btn.png"), true);
+        mainMenuBtn.setLocation(65, 400);
+        mainMenuBtn.setSize(300, 90);
+        mainMenuBtn.addActionListener(this);
+        mainMenuBtn.setMnemonic(69);    // Mnemonic => E
         
-        // TODO specify sliders
-        // loc 70, 80  - size 300, 80
-        // loc 70, 180 - size 300, 80
+        musicSlider.setLocation(65,80);
+        musicSlider.setSize(300, 80);
+        musicSlider.setIcon(context.loadImage("component/button/music_icon.png"));
+        musicSlider.setFocusKey(77);    // Mnemonic => M
+        
+        soundSlider.setLocation(65,180);
+        soundSlider.setSize(300, 80);
+        soundSlider.setIcon(context.loadImage("component/button/sound_icon.png"));
+        soundSlider.setFocusKey(83);    // Mnemonic => S
     }
     
     @Override
     public void actionPerformed(Component comp) {
+        super.actionPerformed(comp);
         if(comp == helpBtn)
             ((ScreenManager)getContext()).switchFrame(ScreenManager.FRAME_HELP);
-        else if(comp == exitBtn)
-            getContext().exit();
-        
-        // TODO add listeners to sliders
+        else if(comp == mainMenuBtn){
+            ((ScreenManager)getContext()).switchScreen(ScreenManager.SCREEN_MAIN_MENU);
+        }
+        // TODO @Buğra add listeners to sliders
     }
 }
